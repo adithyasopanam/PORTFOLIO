878f7223fff213da90895fb99a933b800b6d752a
Adithya S <58964534+adithyasopanam@users.noreply.github.com>
2025-08-14 10:26:07 +0530

    Add files via upload
    Signed-off-by: Adithya S <58964534+adithyasopanam@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // enables static export
+  images: {
+    unoptimized: true, // disable Next.js image optimization
+  },
+  basePath: "PORTFOLIO", // 👈 must match your GitHub repo name
+  assetPrefix: "PORTFOLIO", // 👈 same as above
+};
 
 export default nextConfig;
